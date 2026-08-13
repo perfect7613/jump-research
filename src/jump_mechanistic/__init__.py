98da740b078849a6206a35400ac38400015a1af9
Perfect_7613 <amitjoshi2052003@gmail.com>
2026-08-13 16:30:52 +0530

    feat: add sealed Stage D control executor

--- src/jump_mechanistic/__init__.py
+++ src/jump_mechanistic/__init__.py
@@ -3,6 +3,20 @@
 from .capture import ActivationCapture, CapturePolicy, Timepoint
 from .boundaries import BoundaryManifest, ResolvedPrompt, resolve_boundaries
 from .runtime import HookManifest, MechanisticRuntime, PatchMode, WorldModelRuntimeBinding
+from .stage_d import (
+    LATENT_PERMUTATION_VERSION,
+    STAGE_D_ARMS,
+    STAGE_D_CONTROL_VERSION,
+    STAGE_D_EXECUTION_CONTRACT_SHA256,
+    StageDArmInput,
+    StageDControlEvidence,
+    StageDControlSpec,
+    build_stage_d_control_result,
+    execute_stage_d_control_set,
+    identity_injection,
+    no_z_injection,
+    scrambled_injection,
+)
 
 __all__ = [
     "ActivationCapture",
@@ -15,6 +29,18 @@ __all__ = [
     "MechanisticRuntime",
     "PatchMode",
     "WorldModelRuntimeBinding",
+    "LATENT_PERMUTATION_VERSION",
+    "STAGE_D_ARMS",
+    "STAGE_D_CONTROL_VERSION",
+    "STAGE_D_EXECUTION_CONTRACT_SHA256",
+    "StageDArmInput",
+    "StageDControlEvidence",
+    "StageDControlSpec",
+    "build_stage_d_control_result",
+    "execute_stage_d_control_set",
+    "identity_injection",
+    "no_z_injection",
+    "scrambled_injection",
 ]
 
 __version__ = "0.1.0"
